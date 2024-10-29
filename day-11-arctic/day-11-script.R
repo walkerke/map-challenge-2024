1517cf64a7333030f040808445d82694806257a2
Kyle Walker <kyle@walker-data.com>
2024-10-29 09:43:19 -0500

    publish day 11 materials

---
+++ day-11-arctic/day-11-script.R
@@ -0,0 +1,23 @@
+library(mapgl)
+library(sf)
+library(dplyr)
+library(rnaturalearth)
+
+# Load populated places data from Natural Earth
+populated_places <- ne_download(
+  scale = 10,
+  type = "populated_places_simple"
+)
+
+# Find places north of the Arctic Circle
+arctic_places <- filter(populated_places, latitude > 66.5) |> 
+  transmute(place_name = paste(name, adm0name, sep = ", "))
+
+arctic_map <- mapboxgl(style = mapbox_style("standard-satellite"), center = c(0, 90)) |> 
+  add_markers(
+    data = arctic_places, 
+    color = "lightblue",
+    popup = "place_name"
+  )
+
+htmlwidgets::saveWidget(arctic_map, "day-11-arctic/index.html")
