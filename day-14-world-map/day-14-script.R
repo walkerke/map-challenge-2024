eec04dbdcf69cd088d0b6ac488d40b9175b2e059
Kyle Walker <kyle@walker-data.com>
2024-10-29 10:24:04 -0500

    publish day 14 materials

---
+++ day-14-world-map/day-14-script.R
@@ -0,0 +1,33 @@
+library(idbr)
+library(mapgl)
+library(dplyr)
+
+tfr <- get_idb(
+  country = "all",
+  year = 2024,
+  variables = "tfr",
+  geometry = TRUE
+) 
+
+tfr_map <- mapboxgl(style = carto_style("positron")) |> 
+  add_fill_layer(
+    id = "tfr",
+    source = tfr,
+    fill_color = interpolate(
+      column = "tfr",
+      values = c(1, 7),
+      stops = c("thistle", "indigo")
+    ),
+    tooltip = "tfr"
+  ) |> 
+  add_navigation_control() |> 
+  add_legend(
+    legend_title = "Total fertility rate, 2024",
+    values = c(1, 7),
+    colors = c("thistle",
+    "indigo")
+  )
+
+tfr_map
+
+htmlwidgets::saveWidget(tfr_map, "day-14-world-map/index.html")
