6ed1b60725bcbd1f7792c42ba8f60dd7c894d522
Kyle Walker <kyle@walker-data.com>
2024-11-19 09:17:26 -0600

    commit final map app

--- day-30-final-map/day-30-script.R
+++ day-30-final-map/day-30-script.R
@@ -1,30 +1,67 @@
 library(shiny)
 library(shinyMobile)
 library(mapgl)
+library(mapboxapi)
+
+mb_token <- "pk.eyJ1Ijoia3dhbGtlcnRjdSIsImEiOiJMRk9JSmRvIn0.l1y2jHZ6IARHM_rA1-X45A"
 
 shinyApp(
   ui = f7Page(
     options = list(dark = TRUE, theme = "ios"),
-    title = "mapgl example",
-    f7SingleLayout(
-      navbar = f7Navbar(title = "mapgl example"),
-      toolbar = f7Toolbar(
-        position = "bottom",
-        f7Button("isochrone", "How far can I walk in 10 minutes?")
-      ),
-      mapboxglOutput("map", height = "100vh")
+    title = "Example mobile app with mapgl",
+    f7Navbar(title = "Example mobile app with mapgl"),
+    f7Block(
+      mapboxglOutput("map", height = "70vh")
+    ),
+    f7Block(
+      position = "bottom",
+      f7Button(
+        inputId = "isochrone", 
+        label = "How far can I walk in 10 minutes?",
+        color = "blue",
+        fill = TRUE,
+        rounded = TRUE
+      )
     )
-  ),
+    ),
   server = function(input, output) {
     output$map <- renderMapboxgl({
-      mapboxgl() |> add_geolocate_control(
+      mapboxgl(access_token = mb_token) |> add_geolocate_control(
         show_user_heading = TRUE,
         track_user = TRUE
       )
     })
 
+    output$geolocateInfo <- renderPrint({
+      input$map_geolocate
+    })
+
+
     observeEvent(input$isochrone, {
-      mapboxgl_proxy("map")
+
+      if (!is.null(input$map_geolocate)) {
+        iso <- mb_isochrone(
+          location = c(input$map_center$lng,
+                       input$map_center$lat),
+          time = 10,
+          profile = "walking",
+          access_token = mb_token
+        )
+
+        mapboxgl_proxy("map") |>
+          add_fill_layer(
+            id = "iso",
+            source = iso,
+            fill_opacity = 0.5
+          ) |>
+          fit_bounds(iso, animate = TRUE)
+      } else {
+        f7Dialog(
+          title = "Warning",
+          text = "Please geolocate yourself first before using this feature.",
+          type = "alert"
+        )
+      }
     })
 
   }
