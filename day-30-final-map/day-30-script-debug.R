25831f86f3b3ca7f8bfa6b8f920104a15d164cee
Kyle Walker <kyle@walker-data.com>
2024-11-20 07:51:39 -0600

    update readme

--- day-30-final-map/day-30-script-debug.R
+++ day-30-final-map/day-30-script-debug.R
@@ -9,40 +9,56 @@ shinyApp(
   ui = f7Page(
     options = list(dark = TRUE, theme = "ios"),
     title = "Example mobile app with mapgl",
-    f7Navbar(title = "Example mobile app with mapgl"),
-    f7Block(
-      mapboxglOutput("map", height = "60vh")
-    ),
-    f7Block(
-      position = "bottom",
-      f7Button(
-        inputId = "isochrone",
-        label = "How far can I walk in 10 minutes?",
-        color = "blue",
-        fill = TRUE,
-        rounded = TRUE
+    f7TabLayout(
+      navbar = f7Navbar(title = "Example mobile app with mapgl"),
+      f7Tabs(
+        f7Tab(
+          tabName = "Map",
+          icon = f7Icon("map"),
+          active = TRUE,
+          f7Block(
+            mapboxglOutput("map", height = "85vh")
+          ),
+          f7Block(
+            position = "bottom",
+            f7Button("isochrone", "How far can I walk in 10 minutes?")
+          )
+        ),
+        f7Tab(
+          tabName = "Debug",
+          icon = f7Icon("info_circle"),
+          f7Block(
+            verbatimTextOutput("geolocateInfo"),
+            verbatimTextOutput("geocode")
+          )
+        )
       )
     )
-    ),
+  ),
   server = function(input, output) {
     output$map <- renderMapboxgl({
       mapboxgl(access_token = mb_token) |> add_geolocate_control(
         show_user_heading = TRUE,
         track_user = TRUE
-      )
+      ) |> 
+        add_geocoder_control(position = "top-left", collapsed = TRUE,
+    placeholder = " ")
     })
 
     output$geolocateInfo <- renderPrint({
       input$map_geolocate
     })
 
+    output$geocode <- renderPrint({
+      input$map_geocoder$result
+    })
 
     observeEvent(input$isochrone, {
 
       if (!is.null(input$map_geolocate)) {
         iso <- mb_isochrone(
-          location = c(input$map_geolocate$coords$longitude,
-                       input$map_geolocate$coords$latitude),
+          location = c(input$map_center$lng,
+                       input$map_center$lat),
           time = 10,
           profile = "walking",
           access_token = mb_token
