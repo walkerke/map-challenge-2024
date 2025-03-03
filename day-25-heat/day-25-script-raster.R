0bc00848c84acefafbc4879614269fd8a4047f3a
Kyle Walker <kyle@walker-data.com>
2025-03-02 20:42:11 -0600

    updates

--- day-25-heat/day-25-script-raster.R
+++ day-25-heat/day-25-script-raster.R
@@ -5,21 +5,23 @@ library(dplyr)
 library(mapgl) # remotes::install_github("walkerke/mapgl")
 library(tigris)
 
-# Get Texas boundary
-tx <- states(resolution = "20m") %>%
-  filter(STUSPS == "TX") %>%
+st <- "FL"
+
+# Get boundary
+target_state <- states(resolution = "20m") %>%
+  filter(STUSPS == st) %>%
   st_transform(4326)
 
 # Fetch GridMET data
 tmmx_data <- getGridMET(
-  AOI = tx,
+  AOI = target_state,
   varname = "tmmx",
   startDate = "2025-02-19",
   endDate = "2025-02-19"
 )
 
 tmmx_rast <- tmmx_data$daily_maximum_temperature %>%
-  mask(tx)
+  mask(target_state)
 
 # Convert temperature from Kelvin to Fahrenheit
 tmmx_rast <- (tmmx_rast - 273.15) * 9/5 + 32
@@ -27,7 +29,7 @@ tmmx_rast <- (tmmx_rast - 273.15) * 9/5 + 32
 # Create the map
 heat_map <- mapboxgl(
   style = mapbox_style("light"),
-  bounds = tx,
+  bounds = target_state,
   customAttribution = '<a href="https://www.climatologylab.org/gridmet.html" target="_blank">Data source: GridMET</a>'
 ) %>%
   add_image_source(
@@ -41,7 +43,7 @@ heat_map <- mapboxgl(
     raster_opacity = 0.8
   ) %>%
   add_continuous_legend(
-    legend_title = "High temperature in Texas, Feb 19 2025",
+    legend_title = "High temperature, Feb 19 2025",
     values = c(sprintf("%.1f°F", min(values(tmmx_rast), na.rm = TRUE)),
                sprintf("%.1f°F", max(values(tmmx_rast), na.rm = TRUE))),
     colors = viridisLite::turbo(100)
